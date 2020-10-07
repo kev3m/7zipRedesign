9f0e51b7e81b7edaddf8e4638bbc5b873f91b7d7
Keven Crisostomo <keven_jkc@hotmail.com>
2020-10-07 12:48:17 -0300

    Continuação do corpo

--- index/script.js
+++ index/script.js
@@ -5,11 +5,29 @@ const navSlide = () => {
     const navLinks = document.querySelectorAll('.nav-links li');
 
 
-/* Esconder barra */
-burguer.addEventListener('click', () => {
-    burguer.classList.toggle('active');
-    nav.classList.toggle('active');
-})
+    /* Esconder barra */
+    burguer.addEventListener('click', () => {
+        burguer.classList.toggle('active');
+        nav.classList.toggle('active');
+    })
 
 }
 navSlide();
+
+/* Animação botão download */
+const downbutton = document.querySelectorAll('.download');
+downbutton.forEach(btn => {
+    btn.addEventListener('click', function (e) {
+        let x = e.client - e.target.offsetLeft;
+        let y = e.client - e.target.offsetTop;
+
+        let ripples = document.createElement('span');
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        this.appendChild(ripples)
+
+        setTimeout(() => {
+            ripples.remove();
+        }, 1000);
+    })
+})
